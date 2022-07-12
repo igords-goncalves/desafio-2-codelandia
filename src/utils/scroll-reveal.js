b1c8934bc7869b06bb30d0dab1c6a8541574db66
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-12 14:04:01 -0300

    feat(source): creating a source folder
    Creating a source folder to organIze all files and components

---
+++ src/utils/scroll-reveal.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('.main-produtos', { delay: 100 });
+
+// Só referente a tag que deve ser capturada
---
+++ src/utils/typed.js
@@ -0,0 +1,5 @@
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typedSpeed: 30,
+    showCursor: false
+});
