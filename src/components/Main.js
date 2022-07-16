6c8c2439c1daf6b146b52ff030067d790522cd09
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-16 16:19:15 -0300

    feat(main): build elements of main component

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import { createElements, insertAdjacentTexts, setAttributes } from '../utils/funcions.js'
+
+export function Main() {
+    const div = createElements('div')
+    setAttributes(div, 'class', 'main-cabecalho')
+
+    const text = 'Frete grátis e chinelo de brinde é aqui, aproveite por tempo limitado.'
+
+    const h2 = createElements('h2')
+    insertAdjacentTexts(h2,'beforeend', 'Destaques')
+
+    const span = createElements('span')
+    insertAdjacentTexts(span, 'beforeend', text)
+    setAttributes(span, 'class', 'main-anuncio')
+
+    const element = [h2, span]
+    div.append(...element)
+
+    return div
+}
