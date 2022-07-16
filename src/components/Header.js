e7cd268a2027c510a268765ba5e5371c8275f3c2
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-15 21:38:24 -0300

    feat(header.jsx): create header element in jjs file
    Creatting header element in JavaScript file to be rendered in DOM

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,16 @@
 export function Header() {
-    console.log('Cabecalho')
+    const div = document.createElement('div')
+    div.setAttribute('class', 'main-cabecalho')
+
+    const text = 'Frete grátis e chinelo de brinde é aqui, aproveite por tempo limitado.'
+
+    const h2 = document.createElement('h2')
+    h2.insertAdjacentText('beforeend', 'Destaques')
+
+    const span = document.createElement('span')
+    span.insertAdjacentText('beforeend', text)
+    span.setAttribute('class', 'main-anuncio')
+
+    div.appendChild(h2)
+    div.appendChild(div)
 }
