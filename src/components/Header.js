12a06311a0a0267ad3aafda5ca904e46137811d3
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-16 16:17:35 -0300

    fix(main): fix bug on main component
    Fixing a bug on componet main, all code about main component was on header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,2 @@
 export function Header() {
-    const div = document.createElement('div')
-    div.setAttribute('class', 'main-cabecalho')
-
-    const text = 'Frete grátis e chinelo de brinde é aqui, aproveite por tempo limitado.'
-
-    const h2 = document.createElement('h2')
-    h2.insertAdjacentText('beforeend', 'Destaques')
-
-    const span = document.createElement('span')
-    span.insertAdjacentText('beforeend', text)
-    span.setAttribute('class', 'main-anuncio')
-
-    const element = [h2, span]
-
-    div.append(...element)
-
-    return div
 }
