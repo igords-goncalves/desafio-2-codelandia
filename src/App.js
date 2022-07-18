dd70edb04e80f3183669acc147cbd20b31f58baf
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-18 08:55:10 -0300

    feat(footer): import footer
    Importing footer component and append it on body represented in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { Footer } from "./components/Footer.js";
 import { Header } from "./components/Header.js";
 import { Main } from "./components/Main.js";
 
 
 const elements = [
     Header(),
-    Main()
+    Main(),
+    Footer()
 ]
 
 document.body.append(...elements)
