83bb4912679c9f6cf1964b9afa0c8f1cb30163ac
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-18 08:53:44 -0300

    feat(footer): create footer component
    Creating a footer componenent and imort all packages from utils

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import { createElements, insertAdjacentTexts, setAttributes } from '../utils/funcions.js'
+
+export function Footer() {
+    const footer = createElements('footer')
+
+    console.log('teste')
+    
+    return footer
+}
