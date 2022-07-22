4e86445729e3f23f57f2d9d83d0cc61d1c37fa55
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-22 16:46:51 -0300

    fix(typejs): fix bug on package type js
    Type js package is not working as usual and all HTML of main element code has been deleted.

--- src/utils/typed.js
+++ src/utils/typed.js
@@ -1,5 +1,9 @@
-var typed = new Typed('#typed', {
+import Typed from 'ype.js'
+
+const options = {
     stringsElement: '#typed-strings',
     typedSpeed: 30,
     showCursor: false
-});
+}
+
+var typed = new Typed('#typed', options);
